39d50e59a6e93a8d137c100bd1fdc26a7f558359
guilherme coelho <guilhermeifc7@gmail.com>
2025-03-25 12:00:21 -0300

    Ajusta estilos de responsividade, altera cores de elementos e otimiza navegação no slideshow

--- assets/js/slidePortfolio.js
+++ assets/js/slidePortfolio.js
@@ -1,35 +1,37 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const slides = document.querySelectorAll('.slide');
-    
+document.addEventListener("DOMContentLoaded", function () {
+    const slides = document.querySelectorAll(".slide");
+
     slides.forEach((slide) => {
         let currentIndex = 0;
-        const videos = slide.querySelectorAll('iframe');
-        const nextButton = slide.querySelector('.next');
-        const prevButton = slide.querySelector('.prev');
+        const videos = slide.querySelectorAll("iframe");
+        const nextButton = slide.querySelector(".next");
+        const prevButton = slide.querySelector(".prev");
 
         // Função para mostrar a imagem atual
         function showSlide(index) {
             videos.forEach((video, i) => {
-                video.style.opacity = (i === index) ? '1' : '0';
-                video.style.display = (i === index) ? 'block' : 'none';
+                video.style.opacity = i === index ? "1" : "0";
+                video.style.display = i === index ? "block" : "none";
             });
         }
 
         // Navegação para o próximo slide
-        nextButton.addEventListener('click', () => {
-            console.log('next');
+        nextButton.addEventListener("click", () => {
+            console.log("next");
             currentIndex = (currentIndex + 1) % videos.length;
             showSlide(currentIndex);
         });
 
         // Navegação para o slide anterior
-        prevButton.addEventListener('click', () => {
-            console.log('prev');
+        prevButton.addEventListener("click", () => {
+            console.log("prev");
             currentIndex = (currentIndex - 1 + videos.length) % videos.length;
             showSlide(currentIndex);
         });
 
         // Exibe o slide inicial
-        showSlide(currentIndex);
+        if(window.matchMedia('(max-width: 1024px)').matches){
+            showSlide(currentIndex);
+        }
     });
 });
