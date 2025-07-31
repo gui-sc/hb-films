909326f7b2856531011f1052bb6c923baa6fe822
guilherme coelho <guilhermeifc7@gmail.com>
2025-07-30 22:44:57 -0300

    Adiciona novo vídeo de casamento e corrige título duplicado na lista de vídeos

--- assets/js/slidePortfolio.js
+++ assets/js/slidePortfolio.js
@@ -10,6 +10,7 @@ document.addEventListener("DOMContentLoaded", function () {
         [
             "Casamento",
             "Chá Revelação",
+            "Casamento",
         ],
         [
             "Futevôlei",
